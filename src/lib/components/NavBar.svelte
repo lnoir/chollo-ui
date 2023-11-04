d88b919e3728b5f486bddb44838122c907fc7074
lnoir <gh@latn.black>
2023-11-04 09:48:38 +0000

    style: update root content, tweak nav style

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
 
 </script>
-<nav class="bg-slate-900 p-4 text-md">
+<nav class="bg-slate-900 text-gray-300 p-4 text-md">
   <ul class="flex">
     <li class="mr-6">
-      <a href="/" class="text-white">Dashboard</a>
+      <a href="/">Dashboard</a>
     </li>
     <li class="mr-6">
-	  <a href="/sources" class="text-white">Sources</a>
+	  <a href="/sources">Sources</a>
 	</li>
 	<li class="mr-6">
-	  <a href="/tasks" class="text-white">Tasks</a>
+	  <a href="/tasks">Tasks</a>
 	</li>
   </ul>
 </nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,4 @@
-<script lang="ts">
-	import { onMount } from 'svelte';
-	import { apiService } from '../lib/services/api.service';
-	import type { DocSourceRecord } from '../types';
-	import SourceListItem from '../lib/components/Docs/SourceListItem.svelte';
-
-	let sources: DocSourceRecord[] = [];
-	let ready = false;
-
-	onMount(async () => {
-		sources = await apiService.getDocSources();
-		ready = true;
-	});
-</script>
-
-
-{#if ready}
-<ul class="list-nav p-10">
-	{#each sources as source}
-		<SourceListItem {source} />
-	{/each}
-</ul>
-{/if}
+<div class=" p-8">
+	<h1 class="text-gray-600 text-2xl">Welcome to Chollo</h1>
+	<h2 class="text-gray-300 text-3xl">Let's eat the web!</h2>
+</div>
